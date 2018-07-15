f44e48370c49313a29e4367916c752cdbc7ec38c
steinbrennerGit <steinbrennergit@gmail.com>
2018-07-14 17:17:50 -0700

    added config js, contains database config

---
+++ assets/javascript/config.js
@@ -0,0 +1,8 @@
+var dbConfig = {
+    apiKey: "AIzaSyAjqkNg7cViN8cegM3P-RjsWZr_GcHQvVw",
+    authDomain: "rockpaperscissors-4638c.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-4638c.firebaseio.com",
+    projectId: "rockpaperscissors-4638c",
+    storageBucket: "",
+    messagingSenderId: "801376754914"
+  };
